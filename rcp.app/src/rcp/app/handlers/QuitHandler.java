c265e2f3765d592092d93d6ed50e80e17f9ebb3d
FunThomas424242 <funthomas424242@gmail.com>
2014-04-13 22:18:46 +0200

    Exit Handler fixed
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- rcp.app/src/rcp/app/handlers/QuitHandler.java
+++ rcp.app/src/rcp/app/handlers/QuitHandler.java
@@ -13,16 +13,16 @@ package rcp.app.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.workbench.IWorkbench;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 
 
 public class QuitHandler {
 	@Execute
 	public void execute(IWorkbench workbench, Shell shell){
-		if (MessageDialog.openConfirm(shell, "Confirmation",
-				"Do you want to exit?")) {
-			workbench.close();
-		}
+//		if (MessageDialog.openConfirm(shell, "Confirmation",
+//				"Do you want to exit?")) {
+//			workbench.close();
+//		}
+		workbench.close();
 	}
 }
